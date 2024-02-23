e5048f601bbb832eeef1ea9d54d3a59fa2a3d593
Adrian Skelton <adrianskelton9@gmail.com>
2024-02-23 18:46:06 +0000

    remove secret key

--- nordic_art/settings.py
+++ nordic_art/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w(&322i&89f5p3xo%84)*o-6bau#j^2u(9gb3p%5d!qz(cwh+*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['https://nordic-art-22f58cb8c917.herokuapp.com/' '8000-adrianskelt-adrianproje-kkcdyjvttop.ws-eu107.gitpod.io', '8000-adrianskelt-adrianproje-kkcdyjvttop.ws-eu108.gitpod.io']
 
