869af7716310e6cfb6cbeb015a5a1467cbd0f23b
Adrian Skelton <adrianskelton9@gmail.com>
2024-02-26 08:26:16 +0000

    fix blog view url

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog', views.blog_view, name='blog_view'),  
+    path('blog/', views.blog_view, name='blog_view'),  
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('create_post/', views.new_post, name='create_post'),
 ]
