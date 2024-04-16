a51567dcb03e731387bd9717da981d44f1f940a6
Adrian Skelton <adrianskelton9@gmail.com>
2024-04-16 07:18:24 +0000

    edit email settings to django.core.mail.backends.smtp.EmailBackend

--- nordic_art/settings.py
+++ nordic_art/settings.py
@@ -98,7 +98,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
