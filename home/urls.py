b24ae8460840abfc313ceb209f3e90c12b2eb628
Adrian Skelton <adrianskelton9@gmail.com>
2024-04-22 12:09:29 +0000

    change location of 404 error to home

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
 
-handler404 = 'home.views.custom_404'
-handler400 = 'home.views.custom_400'
-handler500 = 'home.views.custom_500'
-
 urlpatterns = [
     path('', views.index, name='home'),
     path('contact/', views.contact, name='contact_form'),
+    path('404', views.custom_404, name='404'),
+    path('500', views.custom_500, name='500'),
 ]
 
 
