0ea76f53a336f9f243a1edf640badb5d2e526c3f
Adrian Skelton <adrianskelton9@gmail.com>
2024-04-25 10:08:16 +0000

    styling change for signup page

--- nordic_art/settings.py
+++ nordic_art/settings.py
@@ -113,7 +113,7 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
